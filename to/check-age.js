67b17cf7910da7bd3d675aae1a2f71163a136b0c
Angela <113696798+Angela1485@users.noreply.github.com>
2023-06-05 20:42:54 +0200

    Actualizar check-age.js

--- to/check-age.js
+++ to/check-age.js
@@ -1,39 +1,15 @@
 (function () {
-        // Prompt the user for their age
-        var userAge = prompt("Que edad tiene?:");
-
-        // Check if the user's age is less than the legal age
-        var legalAge = 18; // Replace with the legal age in your jurisdiction
-
-        var audio = new Audio();
-        
-        // Function to play the audio
-        function playAudio() {
-          audio.play();
-        }
-        
-        // Function to pause the audio
-        function pauseAudio() {
-          audio.pause();
-        }
-
-        if (userAge < legalAge) {
-          audio.src = "./audio/nene.mp3";
-          alert("Lo siento, aún puede que no conozcas EXTREMODURO");
-        } else {
-          audio.src = "./audio/adulto.mp3";
-          alert("¡Enhorabuena! ¡Superas la mayoría de edad! ¡Tienes gusto musical!");
-        }
-        
-        // Create the play button
-        var playButton = document.createElement("button");
-        playButton.textContent = "Play";
-        playButton.addEventListener("click", playAudio);
-        document.body.appendChild(playButton);
-        
-        // Create the pause button
-        var pauseButton = document.createElement("button");
-        pauseButton.textContent = "Pause";
-        pauseButton.addEventListener("click", pauseAudio);
-        document.body.appendChild(pauseButton);
-      })();
+  // Prompt the user for their age
+  var userAge = prompt("Que edad tiene?:");
+// Check if the user's age is greater than or equal to the legal age
+  var legalAge = 18; // Replace with the legal age in your jurisdiction
+if (userAge >= legalAge) {
+    var audio = new Audio("./audio/adulto.mp3");
+    audio.play();
+    alert("Enhorabuena! superas la mayoria de edad! Tienes gusto musical!");
+  } else {
+    var audio = new Audio("./audio/nene.mp3");
+    audio.play();
+    alert("Lo siento, aun puede que no conozcas EXTREMODURO");
+  }
+})();
