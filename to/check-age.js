123f06b13f501407746da623b88229f5f12f7f2b
Angela <113696798+Angela1485@users.noreply.github.com>
2023-06-05 14:09:41 +0200

    Update check-age.js

--- to/check-age.js
+++ to/check-age.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   // Prompt the user for their age
   var userAge = prompt("Que edad tiene?:");
 
@@ -6,8 +6,12 @@
   var legalAge = 18; // Replace with the legal age in your jurisdiction
 
   if (userAge >= legalAge) {
+    var audio = new Audio("adulto.mp3");
+    audio.play();
     alert("Enhorabuena! superas la mayoria de edad! Tienes gusto musical!");
   } else {
+    var audio = new Audio("nene.mp3");
+    audio.play();
     alert("Lo siento, aun puede que no conozcas EXTREMODURO");
   }
 })();
