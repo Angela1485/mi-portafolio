0b7a0bc4bc7bba99397fe3a3076e1f4e8ef7568f
urielhdz <uriel.hdzc@gmail.com>
2023-07-13 16:01:02 -0600

    Modificar la canción dependiendo de la edad que ingresa el usuario

--- to/check-age.js
+++ to/check-age.js
@@ -2,12 +2,19 @@
   // Prompt the user for their age
   var userAge = prompt("Que edad tiene?:");
 
+  let audioPlayer = document.getElementById("audio-player");
+
+
   // Check if the user's age is greater than or equal to the legal age
   var legalAge = 18; // Replace with the legal age in your jurisdiction
 
   if (userAge >= legalAge) {
     alert("Enhorabuena! superas la mayoria de edad! Tienes gusto musical!");
+    // Set audio source to ./to/audio/nene.mp3
+    audioPlayer.src = "./to/audio/adulto.mp3";
   } else {
     alert("Lo siento, aun puede que no conozcas EXTREMODURO");
+    // Set audio source to ./to/audio/nene.mp3
+    audioPlayer.src = "./to/audio/nene.mp3";
   }
 })();
